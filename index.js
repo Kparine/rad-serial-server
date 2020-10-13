bda1754e511d634743e430316d5fd3a8aaa3900f
Kevin Parine <kparine314@gmail.com>
2020-10-13 11:26:38 -0700

    Update data

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { ApolloServer, gql } = require("apollo-server");
-// const bikes = require("./data.json");
+const bikes = require("./data.json");
 
 const typeDefs = gql`
 	type Bike {
@@ -12,58 +12,6 @@ const typeDefs = gql`
 		bikes: [Bike]
 	}
 `;
-
-const bikes = [
-	{
-		model_cd: {
-			R: "RadRover",
-			M: "RadMini",
-			w: "RadWagon",
-			6: "RadCity 16",
-			9: "RadCity 19",
-			S: "RadCity Stepthru",
-			B: "RadBurro",
-			H: "RadRhino",
-			C: "Large Cargo Box",
-			K: "Small Cargo Box",
-			P: "Pedicab",
-			F: "Flatbed",
-			T: "Truckbed",
-			N: "Insulated Cargo Box",
-			Y: "Runner",
-		},
-	},
-	{
-		month_cd: {
-			1: "January",
-			2: "January",
-			3: "March",
-			4: "April",
-			5: "May",
-			6: "June",
-			7: "July",
-			8: "August",
-			9: "September",
-			O: "October",
-			N: "November",
-			D: "December",
-		},
-	},
-	{
-		model_yr_cd: {
-			A: "2018",
-			B: "2019",
-			C: "2020",
-			D: "2021",
-		},
-	},
-	{
-		factory_cd: {
-			F: "FactoryF",
-			V: "FactoryV",
-		},
-	},
-];
 
 const resolvers = {
 	Query: {
