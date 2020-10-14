488c1a257df0d49368bcfb3edfe6b03447566018
Kevin Parine <kparine314@gmail.com>
2020-10-13 21:14:31 -0700

    digest serial number

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const resolvers = {
 	Query: {
 		bike: (_, args) => {
 			// arg parsing here
+			const res = args.serialNumber.split("");
+			const bikeModel = res[0];
+			const modelYear = res[1];
+			const monthCode = res[2];
+			const manufYear = "20" + res[3] + res[4];
+			const factoryCode = res[5];
+			const version = res[6];
+			const unique = res.slice(6, res.length - 1).join("");
+
 			return { factoryCode: "F" };
 		},
 	},
