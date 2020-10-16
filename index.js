1a48a8f16dc7a76931d9639e77f16f4eb98567ea
Kevin Parine <kparine314@gmail.com>
2020-10-16 00:21:14 -0700

    refactor resolver

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { ApolloServer, ApolloError } = require("apollo-server");
+const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./server/schema");
 const { getPayload } = require("./utils");
 const bikes = require("./data.json");
@@ -7,50 +7,54 @@ const resolvers = {
 	Query: {
 		bikes: (_, args) => {
 			let input = args.serialNumber;
-
 			return input.map((x) => {
-				console.log("x ******------>>>>>>", x);
-
-				const modelCode = x[0];
-				const yearCode = x[1];
-				const monthCode = x[2];
+				let modelCode = x[0];
+				let yearCode = x[1];
+				let monthCode = x[2];
+				let factoryCode = x[5];
 				const manufYear = "20" + x[3] + x[4];
-				const factoryCode = x[5];
 				const version = x[6];
-				// const unique = x.slice(6, x.length - 1).join("");
+				const unique = x.slice(6, x.length);
 
-				const name = bikes.modelCode.find((obj) => {
-					const temp = obj[modelCode];
-					return temp;
-				});
-				const modelName = Object.values(name).toString();
+				const selectValue = (data) => {
+					if (data) return Object.values(data).toString();
+					return;
+				};
 
-				const month = bikes.monthCode.find((obj) => {
-					const temp = obj[monthCode];
-					return temp;
-				});
-				const monthName = Object.values(month).toString();
+				modelCode = selectValue(
+					bikes.modelCode.find((obj) => {
+						return obj[modelCode];
+					})
+				);
 
-				const year = bikes.yearCode.find((obj) => {
-					const temp = obj[yearCode];
-					return temp;
-				});
-				const yearSel = Object.values(year).toString();
+				monthCode = selectValue(
+					bikes.monthCode.find((obj) => {
+						return obj[monthCode];
+					})
+				);
 
-				const factory = bikes.factoryCode.find((obj) => {
-					const temp = obj[factoryCode];
-					return temp;
-				});
-				const factorySel = Object.values(factory).toString();
+				yearCode = selectValue(
+					bikes.yearCode.find((obj) => {
+						return obj[yearCode];
+					})
+				);
+
+				factoryCode = selectValue(
+					bikes.factoryCode.find((obj) => {
+						return obj[factoryCode];
+					})
+				);
 
 				let result = {
-					modelCode: modelName,
-					yearCode: yearSel,
-					monthCode: monthName,
-					factoryCode: factorySel,
+					modelCode,
+					yearCode,
+					monthCode,
+					factoryCode,
 					manufYear,
 					version,
+					unique,
 				};
+
 				return result;
 			});
 		},
@@ -61,15 +65,11 @@ const server = new ApolloServer({
 	typeDefs,
 	resolvers,
 	context: ({ req }) => {
-		try {
-			const token = req.headers.authorization || "";
-			const payload = getPayload(token);
-			return payload;
-		} catch (err) {
-			console.log("err ******------>>>>>>", err);
-		}
+		const token = req.headers.authorization || "";
+		return getPayload(token);
 	},
 });
+
 server.listen().then(({ url }) => {
 	console.log(`🚀 Server launching on ${url}`);
 });
